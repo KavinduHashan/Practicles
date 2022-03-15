b8c852e48b5a42bab64c9a2c0d57e678cee03d62
Kavindu Hashan <89184906+KavinduHashan@users.noreply.github.com>
2022-03-15 12:06:47 +0530

    Add files via upload

---
+++ practicles/practicles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace practicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 1st num : ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter 2nd num : ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int swap = 0;
+            //swap
+            swap = n1;
+            n1 = n2;
+            n2=swap;
+
+            Console.WriteLine("Enter 1st num = " + n1);
+            Console.WriteLine("Enter 2nd num = " + n2);
+
+            Console.ReadKey();
+        }
+    }
+}
